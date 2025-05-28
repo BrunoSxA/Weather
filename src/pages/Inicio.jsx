4627aed7833dc2b38efe70dfdbebb4d0835cd837
BrunoSxA <bruncab2424@gmail.com>
2025-05-27 18:11:54 -0600

    estilos de tarjetas

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -4,7 +4,13 @@ import Layout from '../layouts/Layout'
 export default function Inicio() {
   return (
     <Layout>
-     
+      <div className='flex flex-col items-center justify-center h-[650px]'>
+        <div class="w-80% overflow-hidden">
+          <div className="flex flex-wrap justify-center gap-4">
+            <div class="min-w-[200px] bg-[linear-gradient(rgba(0,0,0,0.7),rgba(139,69,19,0.7))] border rounded-2xl flex justify-center items-center text-2xl text-center p-[20px] cursor-pointer">A Nightmare on Elm Street</div>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
